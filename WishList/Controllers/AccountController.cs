cf2758e06750ed8f6c12c3a85f7da1c912e9b9f0
Cathal Reddin <Creddin@lgma.ie>
2020-11-04 08:54:06 +0000

    Complete Module 3 part 3

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -11,10 +11,15 @@ namespace WishList.Controllers
 {
     [Authorize]
     public class AccountController : Controller
-    {
+    {       
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
-        
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
