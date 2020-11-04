64c9894e239ecd7b4d1a798a9eb29a7d879a92dd
Cathal Reddin <Creddin@lgma.ie>
2020-11-04 09:23:47 +0000

    Module 4 Part 3 Completed

--- WishList/Models/AccountViewModels/RegisterViewModel.cs
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -12,7 +12,7 @@ namespace WishList.Models.AccountViewModels
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [StringLength(100)]
+        [StringLength(100, MinimumLength = 8)]
         [DataType(DataType.Password)]
         public string  Password { get; set; }
         [Required]
